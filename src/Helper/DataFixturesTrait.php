8f69809e7c36d9130eebad70c27a63d78eb50a78
yethee <dev@airmedia.msk.ru>
2019-08-05 10:46:49 +0300

    Drop support shared fixtures from DataFixturesTrait

--- src/Helper/DataFixturesTrait.php
+++ src/Helper/DataFixturesTrait.php
@@ -9,44 +9,11 @@ use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\DataFixtures\Loader;
 use Doctrine\ORM\EntityManagerInterface;
-use function count;
 use function is_string;
 
 trait DataFixturesTrait
 {
-    /**
-     * @var FixtureInterface[]|string[]
-     */
-    public static $sharedFixtures = [];
-
-    /**
-     * @var bool
-     */
-    private static $sharedFixturesLoaded = false;
-
-    public static function setUpBeforeClass(): void
-    {
-        self::$sharedFixturesLoaded = false;
-
-        parent::setUpBeforeClass();
-    }
-
     abstract protected function getEntityManager(): EntityManagerInterface;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        if (self::$sharedFixturesLoaded) {
-            return;
-        }
-
-        if (count(self::$sharedFixtures) > 0) {
-            $this->loadFixtures(self::$sharedFixtures);
-        }
-
-        self::$sharedFixturesLoaded = true;
-    }
 
     /**
      * @param FixtureInterface[]|string[] $fixtures
@@ -66,8 +33,6 @@ trait DataFixturesTrait
 
         $executor = $this->getExecutor($this->getEntityManager());
         $executor->execute($loader->getFixtures(), $append);
-
-        self::$sharedFixturesLoaded = true;
     }
 
     protected function getExecutor(EntityManagerInterface $em): ORMExecutor
